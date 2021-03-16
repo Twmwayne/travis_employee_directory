65ce548ca4a99bdecc74c53e523b4c68e45278d0
Travis Mendenhall <twmwayne@gmail.com>
2021-03-16 16:35:35 -0600

    created userContainer class

---
+++ my-app/src/components/UserContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import API from "../utils/API";
+
+class UserContainer extends React.Component {
+    state = {
+        users: [],
+        search: "",
+        sortDirection: "",
+        col: ""
+    };
+
+    
+}
