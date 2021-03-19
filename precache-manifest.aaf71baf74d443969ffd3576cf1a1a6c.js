fb5675035a46973474337e01f4fdd4eadb99ea5e
Travis Mendenhall <twmwayne@gmail.com>
2021-03-19 14:19:38 -0600

    Updates

---
+++ precache-manifest.aaf71baf74d443969ffd3576cf1a1a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2403b7ca30af935d9abc7d7492405e",
+    "url": "/Twmwayne/travis_employee_directory/index.html"
+  },
+  {
+    "revision": "abe9069e02f7c8283194",
+    "url": "/Twmwayne/travis_employee_directory/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "28fc9062f61fda9b7a37",
+    "url": "/Twmwayne/travis_employee_directory/static/css/main.66e98012.chunk.css"
+  },
+  {
+    "revision": "abe9069e02f7c8283194",
+    "url": "/Twmwayne/travis_employee_directory/static/js/2.2a2dc6f6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Twmwayne/travis_employee_directory/static/js/2.2a2dc6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28fc9062f61fda9b7a37",
+    "url": "/Twmwayne/travis_employee_directory/static/js/main.cd247935.chunk.js"
+  },
+  {
+    "revision": "8636df0606685c9e5b75",
+    "url": "/Twmwayne/travis_employee_directory/static/js/runtime-main.094ec064.js"
+  }
+]);
